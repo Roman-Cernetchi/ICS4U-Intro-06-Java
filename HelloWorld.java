574613fc38e3e6ea02f60d52c8fdce801a2c4154
Ubuntu <ubuntu@ip-172-31-89-209.ec2.internal>
2021-11-18 05:08:06 +0000

    added comments

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,20 +1,11 @@
-/******************************************************************************
- *  Compilation:  javac HelloWorld.java
- *  Execution:    java HelloWorld
+/**
+ * The HelloWorld program implements an application that
+ * simply displays "Hello World!" to the standard output.
  *
- *  Prints "Hello, World". By tradition, this is everyone's first program.
- *
- *  % java HelloWorld
- *  Hello, World
- *
- *  These 17 lines of text are comments. They are not part of the program;
- *  they serve to remind us about its properties. The first two lines tell
- *  us what to type to compile and test the program. The next line describes
- *  the purpose of the program. The next few lines give a sample execution
- *  of the program and the resulting output. We will always include such 
- *  lines in our programs and encourage you to do the same.
- *
- ******************************************************************************/
+ * @author  Roman Cernetchi
+ * @version 1.0
+ * @since   2020-01-01
+ */
 
 public class HelloWorld {
 
